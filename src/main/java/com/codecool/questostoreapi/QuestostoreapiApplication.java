96296dac3d0c348126ceaab811e7e4bfb648f6a8
PatrykTopolski <venirro@gmail.com>
2019-11-04 12:55:57 +0100

    Initial commit

---
+++ src/main/java/com/codecool/questostoreapi/QuestostoreapiApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.questostoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestostoreapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestostoreapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/questostoreapi/QuestostoreapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.questostoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestostoreapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
