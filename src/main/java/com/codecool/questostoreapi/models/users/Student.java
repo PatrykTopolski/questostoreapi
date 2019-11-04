f7352fa04cac112184611a05699dd14c1c99f150
PatrykTopolski <venirro@gmail.com>
2019-11-04 14:54:36 +0100

    quick fix to getters and setters inn Student entity

--- src/main/java/com/codecool/questostoreapi/models/users/Student.java
+++ src/main/java/com/codecool/questostoreapi/models/users/Student.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-public class Student extends User {
+public class Student  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,87 +22,14 @@ public class Student extends User {
     private String email;
     private String address;
 
-    @Override
-    public String getLogin() {
-        return login;
-    }
-
-    @Override
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String getFirstName() {
-        return firstName;
-    }
-
-    @Override
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @Override
-    public String getLastName() {
-        return lastName;
-    }
-
-    @Override
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-
-    @Override
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String getAddress() {
-        return address;
-    }
-
-    @Override
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
     public int getId() {
         return id;
     }
 
-    @Override
     public void setId(int id) {
         this.id = id;
     }
 
-    @Override
     public int getAmountOfCoins() {
         return amountOfCoins;
     }
@@ -111,12 +38,67 @@ public class Student extends User {
         this.amountOfCoins = amountOfCoins;
     }
 
-    @Override
     public int getLvlOfExp() {
         return lvlOfExp;
     }
 
     public void setLvlOfExp(int lvlOfExp) {
         this.lvlOfExp = lvlOfExp;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
