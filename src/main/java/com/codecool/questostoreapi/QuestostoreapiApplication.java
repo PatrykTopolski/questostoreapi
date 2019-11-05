1db2e5d760dfed0b38c8baf75405df56ae9358c0
PatrykTopolski <venirro@gmail.com>
2019-11-05 13:34:31 +0100

    added few new users to data base

--- src/main/java/com/codecool/questostoreapi/QuestostoreapiApplication.java
+++ src/main/java/com/codecool/questostoreapi/QuestostoreapiApplication.java
@@ -8,6 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @SpringBootApplication
 public class QuestostoreapiApplication {
 
@@ -20,10 +25,10 @@ public class QuestostoreapiApplication {
         return (args) -> {
             // save a few customers
             sr.save(new Student(50,5, "student", "student", "marek", "ziomek", "544032056", "student@student", "gdzies tam 15/4" ));
+            sr.save(new Student(43,3, "gdyrye", "hasło2", "kuba", "random", "456789123", "next@mail", "somewhere 58/1" ));
+            sr.save(new Student(76,1, "student10", "passowrd8", "gregory", "marier", "543876098", "random@mail", "bohemia 33/4" ));
 
             // fetch all customers
-
-
 
 
         };
--- src/main/java/com/codecool/questostoreapi/models/users/Mentor.java
+++ src/main/java/com/codecool/questostoreapi/models/users/Mentor.java
@@ -11,8 +11,8 @@ public class Mentor  {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-//    @OneToMany(mappedBy = "Mentor")
-//    private Set<Student> students;
+    @OneToMany(mappedBy = "Mentor")
+    private Set<Student> students;
     private String login;
     private String password;
     @Column(table = "mentor_personals")
@@ -27,6 +27,17 @@ public class Mentor  {
     private String address;
 
 
+    public Mentor(Set<Student> students, String login, String password, String firstName, String lastName, String phoneNum, String email, String address) {
+        this.students = students;
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.address = address;
+    }
+
     public int getId() {
         return id;
     }
@@ -35,13 +46,13 @@ public class Mentor  {
         this.id = id;
     }
 
-//    public Set<Student> getStudents() {
-//        return students;
-//    }
-//
-//    public void setStudents(Set<Student> students) {
-//        this.students = students;
-//    }
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
 
     public String getLogin() {
         return login;
