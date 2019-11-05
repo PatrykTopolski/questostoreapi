ef1ec89d0ac9d1fbf0bb45f8d9376b1e8b28b24f
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-05 13:12:52 +0100

    add mentohd to poplate db into InitializeBean Constructor

--- src/main/java/com/codecool/questostoreapi/service/InitializerBean.java
+++ src/main/java/com/codecool/questostoreapi/service/InitializerBean.java
@@ -19,6 +19,8 @@ public class InitializerBean {
     MentorRepository mentorRepo;
 
     public InitializerBean(){
+        createArtifacts();
+        createMentor();
     }
 
     public void createArtifacts(){
