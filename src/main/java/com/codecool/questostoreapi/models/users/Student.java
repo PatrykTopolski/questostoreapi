23042ff454535ed755cb545e8bd1d0d0f5ed7e05
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-06 09:20:30 +0100

    removed unneeded import from student class

--- src/main/java/com/codecool/questostoreapi/models/users/Student.java
+++ src/main/java/com/codecool/questostoreapi/models/users/Student.java
@@ -1,8 +1,6 @@
 package com.codecool.questostoreapi.models.users;
 
 import com.codecool.questostoreapi.models.items.Artifact;
-import com.codecool.questostoreapi.models.items.Quest;
-
 import javax.persistence.*;
 import java.util.List;
 
