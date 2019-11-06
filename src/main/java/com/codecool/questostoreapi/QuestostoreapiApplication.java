3ed8af1b0b2743e80a1ac0f6625e613e04480fe4
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-06 15:06:21 +0100

    try to make logging but fail again

--- src/main/java/com/codecool/questostoreapi/QuestostoreapiApplication.java
+++ src/main/java/com/codecool/questostoreapi/QuestostoreapiApplication.java
@@ -3,6 +3,9 @@ package com.codecool.questostoreapi;
 import com.codecool.questostoreapi.models.users.Student;
 import com.codecool.questostoreapi.repositories.MentorRepository;
 import com.codecool.questostoreapi.repositories.StudentRepository;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +18,10 @@ import java.util.Set;
 
 @SpringBootApplication
 public class QuestostoreapiApplication {
+    //private static final Logger logger = LogManager.getLogger(QuestostoreapiApplication.class);
 
     public static void main(String[] args) {
+
         SpringApplication.run(QuestostoreapiApplication.class, args);
     }
 
@@ -27,6 +32,14 @@ public class QuestostoreapiApplication {
 //            sr.save(new Student(50,5, "student", "student", "marek", "ziomek", "544032056", "student@student", "gdzies tam 15/4" ));
 //            // fetch all customers
 //        };
+//    }
+
+//    public void run(ApplicationArguments applicationArguments) throws Exception {
+//        logger.debug("Debugging log");
+//        logger.info("Info log");
+//        logger.warn("Hey, This is a warning!");
+//        logger.error("Oops! We have an Error. OK");
+//        logger.fatal("Damn! Fatal error. Please fix me.");
 //    }
 
 }
