b3032aa069781b4b599f8a6fe6abe8886e451547
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-07 09:36:47 +0100

    logging info in mentor class

--- src/main/java/com/codecool/questostoreapi/api/MentorServiceREST.java
+++ src/main/java/com/codecool/questostoreapi/api/MentorServiceREST.java
@@ -1,8 +1,8 @@
 package com.codecool.questostoreapi.api;
 
-import com.codecool.questostoreapi.models.items.Artifact;
 import com.codecool.questostoreapi.models.users.Mentor;
 import com.codecool.questostoreapi.repositories.MentorRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,28 +13,35 @@ public class MentorServiceREST {
     @Autowired
     private MentorRepository repository;
 
+    private Logger logger = Logger.getLogger(MentorServiceREST.class.getName());
+
     @GetMapping("/api/mentor")
     public List<Mentor> getAllMentors(){
+        logger.info("get mentors request");
         return repository.findAll();
     }
 
     @GetMapping("/api/mentor/{mentorId}")
-    Mentor getMentorById(@PathVariable String mentorId){
+    Mentor getMentorById(@PathVariable String mentorId)
+    {   logger.info("get mentor request");
         return repository.getById(Integer.parseInt(mentorId));
     }
 
     @PostMapping("api/mentor")
     Mentor newMentor(@RequestBody Mentor newMentor){
+        logger.info("post mentor request");
         return repository.save(newMentor);
     }
 
     @DeleteMapping("/api/mentor/{id}")
     void deleteById(@PathVariable int id){
+        logger.info("delete mentor request");
         repository.deleteById(id);
     }
 
     @PutMapping("/api/mentor/{id}")
     Mentor updateMentor(@RequestBody Mentor updatedMentor, @PathVariable int id){
+        logger.info("put mentor request");
         return repository.findById(id)
                 .map(mentor -> {
                     mentor.setFirstName(updatedMentor.getFirstName());
