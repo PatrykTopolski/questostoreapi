2bac036326c02ad3c116bc42f10d5cf09502798b
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-11-11 15:20:18 +0100

    getMEntor throws exception

--- src/main/java/com/codecool/questostoreapi/repositories/MentorRepository.java
+++ src/main/java/com/codecool/questostoreapi/repositories/MentorRepository.java
@@ -3,8 +3,10 @@ package com.codecool.questostoreapi.repositories;
 import com.codecool.questostoreapi.models.users.Mentor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.persistence.EntityNotFoundException;
+
 public interface MentorRepository extends JpaRepository<Mentor, Integer> {
-    public Mentor getById(Integer id);
+    public Mentor getById(Integer id) throws EntityNotFoundException;
 
     }
 
